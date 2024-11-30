2027d831ff0f727b50b9280301d51605e4da2665
mfbevan <matthewbevan00@gmail.com>
2024-11-30 16:16:54 +1000

    feat: ✨ poc warning banner

---
+++ src/components/banners/poc-banner.tsx
@@ -0,0 +1,17 @@
+import { TriangleAlert } from "lucide-react";
+
+export const PocBanner = () => {
+  return (
+    <div className="flex w-full max-w-md items-center gap-4 border p-4">
+      <div>
+        <TriangleAlert className="size-6" />
+      </div>
+      <div className="flex flex-col">
+        <div className="text-xs font-medium text-muted-foreground">
+          This is a proof of concept and should not be used in production.
+          Don&apos;t blame me if you lose funds.
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -2,14 +2,18 @@
 import { type FC } from "react";
 import Link from "next/link";
 
+import { PocBanner } from "../banners/poc-banner";
+
 import { ColorModeButton } from "./color-mode-button";
 export const Footer: FC = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 w-screen border-b p-2 font-mono">
-      <footer className="max-w-screen mx-auto flex w-full items-center justify-between">
+      <footer className="max-w-screen mx-auto flex w-full flex-col items-center justify-between md:flex-row">
         <p className="p-2 text-xs text-muted-foreground">
           Unofficial LayerZero Bridge UI
         </p>
+
+        <PocBanner />
 
         <div className="flex items-center gap-4">
           <p className="text-xs text-muted-foreground">
