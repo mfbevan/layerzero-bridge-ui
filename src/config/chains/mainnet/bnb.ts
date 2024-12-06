87fc7333d25deb5404be22133e80a0366f3f25cd
mfbevan <matthewbevan00@gmail.com>
2024-12-06 14:06:34 +1000

    fix: 🐛 bnb chain config

--- src/config/chains/mainnet/bnb.ts
+++ src/config/chains/mainnet/bnb.ts
@@ -6,7 +6,7 @@ export const bnb: LayerZeroChain = defineLZChain({
   id: 30202,
   name: "BNB",
   icon: "https://icons-ckg.pages.dev/lz-scan/networks/bnb.svg",
-  endpoint: EndpointId.BNB,
+  endpoint: EndpointId.BSC_MAINNET,
   rpc: "https://binance.llamarpc.com",
   testnet: false,
 });
--- src/config/chains/testnet/bnb-testnet.ts
+++ src/config/chains/testnet/bnb-testnet.ts
@@ -6,7 +6,7 @@ export const bscTestnet: LayerZeroChain = defineLZChain({
   id: 40102,
   name: "BNB Testnet",
   icon: "https://icons-ckg.pages.dev/lz-scan/networks/bnb.svg",
-  endpoint: EndpointId.BNB_TESTNET,
+  endpoint: EndpointId.BSC_TESTNET,
   rpc: "https://bsc-testnet-rpc.publicnode.com",
   testnet: true,
 });
