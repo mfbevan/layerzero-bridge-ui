d1eb1d2bfbd730aeea9344765ee6f1a12ec02582
mfbevan <matthewbevan00@gmail.com>
2025-03-25 18:59:28 +1000

    fix: 🐛 bnb chainids

--- src/config/chains/mainnet/bnb.ts
+++ src/config/chains/mainnet/bnb.ts
@@ -3,7 +3,7 @@ import { EndpointId } from "@layerzerolabs/lz-definitions";
 import { defineLZChain, type LayerZeroChain } from "../types";
 
 export const bnb: LayerZeroChain = defineLZChain({
-  id: 30202,
+  id: 56,
   name: "BNB",
   icon: "https://icons-ckg.pages.dev/lz-scan/networks/bnb.svg",
   endpoint: EndpointId.BSC_MAINNET,
--- src/config/chains/testnet/bnb-testnet.ts
+++ src/config/chains/testnet/bnb-testnet.ts
@@ -3,7 +3,7 @@ import { EndpointId } from "@layerzerolabs/lz-definitions";
 import { defineLZChain, type LayerZeroChain } from "../types";
 
 export const bscTestnet: LayerZeroChain = defineLZChain({
-  id: 40102,
+  id: 97,
   name: "BNB Testnet",
   icon: "https://icons-ckg.pages.dev/lz-scan/networks/bnb.svg",
   endpoint: EndpointId.BSC_TESTNET,
